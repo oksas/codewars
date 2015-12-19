bf4af9df4fc65ce5236cd0357422ab6e324f7d83
John Oksasoglu <john.oksasoglu@gmail.com>
2015-12-19 13:53:31 -0700

    Add another solution

---
+++ 7kyu/largest-pair-sum-in-array.js
@@ -0,0 +1,16 @@
+/*
+  Kata written by: sudheer594
+  =====
+  Given an array of numbers, find the largest pair sum in the array.
+
+  For example
+
+  [10,14,2,23,19] should return 42 (i.e. sum of 23,19)
+  [99,2,2,23,19] should return 122 (i.e. sum of 99,23)
+  Input array contains minimum two elements and every element is a number.
+*/
+
+function largestPairSum(numbers) {
+  var sorted = numbers.sort(function(a, b) { return b - a; });
+  return sorted[0] + sorted[1];
+}
